d096aca8db8408933817cd8ff6426870d2996f33
suldinaelena <elenasuldina@mail.ru>
2017-09-07 15:37:39 +0300

    try

---
+++ src/main/java/com/company/FileWorker.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import com.opencsv.CSVWriter;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.String.format;
+
+public class FileWorker {
+    public static void main(String[] args) throws IOException {
+        String fileName = "src/main/resources/chr1.sjubst.fa";
+        String pattern = "AGAAGGGA";
+        int subsstringsCout = 0;
+
+        List<String> linesFromFile = readFromFile(fileName);
+
+        for(String item: linesFromFile) {
+            subsstringsCout += getCountSubstringInString(item, pattern);
+        }
+
+        System.out.println(subsstringsCout);
+    }
+
+    public static List<String> readFromFile(String fileName) throws IOException{
+        List<String> linkedList = new LinkedList<>();
+
+        try (BufferedReader bfreader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)))) {
+            String strLine;
+            while ((strLine = bfreader.readLine()) != null) {
+                linkedList.add(strLine);
+            }
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException(format("file [%s] not found", fileName));
+        }
+        return linkedList;
+    }
+
+    public static int getCountSubstringInString(String string, String patternValue) {
+        int counter = 0;
+        Pattern pattern = Pattern.compile(patternValue, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(string);
+        while (matcher.find()) {
+            counter++;
+        }
+        return counter;
+    }
+}
